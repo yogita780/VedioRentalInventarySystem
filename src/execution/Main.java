a857f11912b80a5ec22fb31d0aaa116b536e3c46
Yogita Balyan <yogita.balyan_cs19@gla.ac.in>
2020-08-21 19:17:25 +0530

    add Main.java

---
+++ src/execution/Main.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Yogita Balyan(Yogita780)
+ *   Date: 21-08-2020
+ *   Time: 19:16
+ *   File: Main.java
+ */
+
+package execution;
+
+public class Main {
+}
